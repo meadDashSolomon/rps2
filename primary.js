f651c9cd7549caa5ca11b645f5ea7f2bba935dcc
Mead Solomon <mead93@gmail.com>
2022-11-28 12:49:24 -0600

    Created game function and playRound function

--- primary.js
+++ primary.js
@@ -1,5 +1,18 @@
 const choices = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice () {
-    return choices[Math.floor(Math.random() * 3)];
+function game () {
+    for (let i = 0; i < 5; i++); {
+        playRound ();
+    }
 }
+
+function playRound () {
+    const computerChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice();
+}
+
+function getComputerChoice () {
+    return choices[Math.floor(Math.random()*3)];
+}
+
+game();
