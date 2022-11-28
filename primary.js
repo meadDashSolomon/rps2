5bbe94a4fd4805570f9c06d9519cc11b80204a1c
Mead Solomon <mead93@gmail.com>
2022-11-28 13:02:30 -0600

    created getPlayerChoice function with prompt and toLowerCase

--- primary.js
+++ primary.js
@@ -7,12 +7,24 @@ function game () {
 }
 
 function playRound () {
-    const computerChoice = getComputerChoice();
-    const playerChoice = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    const playerSelection = getPlayerChoice();
+   // const winner = checkWinner(playerSelection, computerSelection);
+    //winners.push(winner);
 }
 
 function getComputerChoice () {
     return choices[Math.floor(Math.random()*3)];
 }
 
+function getPlayerChoice () {
+    let input = prompt("Type rock, paper, or scissors");
+    while (input == null) {
+        prompt("Type rock, paper, or scissors");
+    }
+    input = input.toLowerCase;
+}
+
 game();
+
+console.log(game);
